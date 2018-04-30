2b6b223095fa8dd5dd7faadb574ea26ec87689f8
Wangyang <d13532641570@sina.com>
2018-04-30 14:32:55 +0800

    Add Format & List_Tuple practice

---
+++ List_Tuple_practice.py
@@ -0,0 +1,15 @@
+# -*- conding:utf-8 -*-
+#功能：用索引取出list的指定元素
+
+L = [
+    ['Apple','Google','Microsoft'],
+    ['Java','Python','Ruby','PHP'],
+    ['Adam','Bart','Lisa']
+]
+
+#打印Apple
+print(L[0][0])
+#打印Python
+print(L[1][1])
+#打印Lisa
+print(L[2][2])
