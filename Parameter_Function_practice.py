c7ce61b77130b48a67c8e685c69f64d6384f20b8
Wangyang <d13532641570@sina.com>
2018-05-02 01:08:06 +0800

    add Parameter_Function practice

---
+++ Parameter_Function_practice.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- conding:utf-8 -*-
+#功能：可接收一个或多个数并计算乘积
+def product(x,y):
+    return x*y
+
+
+def product(*numbers):
+    mul = 1
+    for num in numbers:
+        mul = num * mul
+    return mul
+
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试成功!')
+    except TypeError:
+        print('测试失败!')
+
