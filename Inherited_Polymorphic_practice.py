e7423d50efc09758725749d5a7803ce77f840e16
Wangyang <d13532641570@sina.com>
2018-05-27 22:07:00 +0800

    Update Inherited_Polymorphic practice

--- Inherited_Polymorphic_practice.py
+++ Inherited_Polymorphic_practice.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-#功能
+#功能：继承和多态
+
+class Animal(object):
+    def run(self):
+        print("Animal is running...")
+
+class Dog(Animal):
+    def run(self):
+        print("Dog is running...")
+
+class Cat(Animal):
+    def run(self):
+        print("Cat is running...")
+
+class Tortoise(Animal):
+    def run(self):
+        print("Tortoise is running...")
+
+def run_twice(animal):
+    animal.run()
+
+run_twice(Animal())
+run_twice(Dog())
+run_twice(Cat())
+run_twice(Tortoise())
