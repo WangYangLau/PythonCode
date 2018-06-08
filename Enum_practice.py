a453dc7a6f5dfa2ddf43f9abba598449c438cd1a
Wangyang <d13532641570@sina.com>
2018-06-08 17:29:58 +0800

    add Emun practice

---
+++ Enum_practice.py
@@ -0,0 +1,22 @@
+#!/usr/env/bin python3
+# -*- coding:utf-8 -*-
+#功能：把Student的gender属性改造为枚举类型，可以避免使用字符串
+
+from enum import  Enum
+
+class Gender(Enum):
+    Male = 0
+    Female = 1
+
+class Student(object):
+    def __init__(self,name,value):
+        self.name = name
+        self.gender = Gender(value)
+
+bart = Student('Bart', 0)
+print(bart.gender)
+print(Gender.Male)
+if bart.gender == Gender.Male:
+    print('测试通过!')
+else:
+    print('测试失败!')
